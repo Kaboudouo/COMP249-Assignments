e59ce81f021883448bce191fc99fe36dfcf2ee5a
Kaboudouo <chotto60@hotmail.com>
2023-01-23 18:26:41 -0500

    Added game engine

--- PlayLadderAndSnake.java
+++ PlayLadderAndSnake.java
@@ -21,17 +21,31 @@ class Player{
 }
 
 class Tiles{
-    public boolean hasPlayer;
+    public Player currentPlayer;
     public boolean hasSnake;
     public boolean hasLadder;
     public int tileNumber;
 
+    public int[] tileIdx(int _tileNum){
+        int[] idx = new int[2];
+        for (int i = 0; i < 10; i++){
+            for (int j = 0; j < 10; j++){
+                if (tileNumber == _tileNum){
+                    idx[0] = i;
+                    idx[1] = j;
+                }
+            }
+        }
+        return idx;
+    }
+
     public Tiles (boolean _hasSnake, boolean _hasLadder, int _tileNumber){
-        hasPlayer = false;
+        currentPlayer = null;
         hasSnake = _hasSnake;
         hasLadder = _hasLadder;
         tileNumber = _tileNumber;
     }
+
 }
 
 class LadderAndSnake {
@@ -44,8 +58,9 @@ class LadderAndSnake {
     private int[] snakeHeads =  {16, 48, 64, 79, 93, 95, 97, 98};
     private int[] snakeFoots =  {6,  30, 60, 19, 68, 24, 76, 78};
     private int[] ladderFoots = {1,  4,  9,  21, 28, 36, 51,  71, 80};
-    private int[] ladderHeads = {38, 14, 31, 42, 84, 44, 667, 91, 100};
+    private int[] ladderHeads = {38, 14, 31, 42, 84, 44, 67, 91, 100};
 
+    public int turnCount = 0;
 
     // Sets Number For Each Tile
     private void initializeBoard(){
@@ -68,8 +83,34 @@ class LadderAndSnake {
         return false;
     }
 
-    private void movePlayer(){
+    private void movePlayer(Player player){
+
+        int lastPos = player.position;
+        player.position += player.currentRoll;
+
+        if (player.position > 100){
+            player.position = 200 - player.position;
+        }
         
+        for(int i = 0; i < snakeHeads.length; i++){
+            if (player.position == snakeHeads[i]){
+                player.position = snakeFoots[i];
+                System.out.print(player.name + " has folled a " + player.currentRoll + " but landed on a snake!;");
+                System.out.println(" ssssslid from " + snakeHeads[i] + " to " + player.position +  ".");
+                return;
+            }
+        }
+
+        for (int i = 0; i < ladderFoots.length; i++){
+            if (player.position == ladderFoots[i]){
+                player.position = ladderHeads[i];
+                System.out.print(player.name + " has folled a " + player.currentRoll + " and found a ladder!");
+                System.out.println(" climbed from " + ladderFoots[i] + " to " + player.position + ".");
+                return;
+            }
+        }
+
+        System.out.println(player.name + " has folled a " + player.currentRoll + "; went from " + lastPos + " to " + player.position + ".");  
     }
 
     //Handles roll priority and player order
@@ -127,7 +168,7 @@ class LadderAndSnake {
 
     // Game Engine
     public void play(){
-
+        System.out.println(tiles[3][6].tileNumber);
         // Initialize players
         for (int i = 0; i < playerCount; i++){
             players.add(new Player("Player_" + Integer.toString(i+1)));
@@ -154,9 +195,22 @@ class LadderAndSnake {
         System.out.println(". It took " + attempt_count + " attempts before a decision could be made.");
 
         // Game Logic
-        //
-        //
-        // -----------------------
+        System.out.println("------------------------------------\n     The Game Will Now Start     \n------------------------------------");
+        while(true){
+            turnCount++;
+            System.out.println("\n---------------Turn " + turnCount + "---------------");
+            for (Player player : orderedPlayers){
+                player.currentRoll = flipDice();
+                movePlayer(player);
+
+                if(player.position == 100){
+                    System.out.println(player.name + " has reached the 100th square!");
+                    System.out.println("Congratulations " + player.name + "!\n------------------------------\nThank you for playing Kab's amazing Snakes&Ladders.");
+                    System.exit(0);
+                }
+            }
+        }
+
     }
 
     // Default Constructor
@@ -179,21 +233,28 @@ class LadderAndSnake {
 public class PlayLadderAndSnake {
     public static void main(String[] args){
         Scanner input = new Scanner(System.in);
-        System.out.println("Welcome to Snakes and Ladders. Pleaser enter the number of players:\n");
+        System.out.print("Welcome to Snakes and Ladders. Pleaser enter the number of players: ");
 
         int player_count;
+
+        if (!input.hasNextInt()){
+            System.out.println("Error. Please input a valid integer. Game will now exit.");
+            System.exit(0);
+        }
         player_count = input.nextInt();
 
         input.close();
 
+        System.out.println("\n");
+
         // Comment this block for more than 2 players
-        if (player_count > 2){
-            System.out.println("Initialization was attempted for " + player_count + " players; however, this is only expected for an extended version the game. Value will be set to 2");
-            player_count = 2;
-        } else if (player_count < 2){
-            System.out.println("Error: Cannot execute the game with less than 2 players! Will exit.");
-            System.exit(0);
-        }
+        // if (player_count > 2){
+        //     System.out.println("Initialization was attempted for " + player_count + " players; however, this is only expected for an extended version the game. Value will be set to 2");
+        //     player_count = 2;
+        // } else if (player_count < 2){
+        //     System.out.println("Error: Cannot execute the game with less than 2 players! Will exit.");
+        //     System.exit(0);
+        // }
 
         LadderAndSnake GameBoard = new LadderAndSnake(player_count);
         GameBoard.play();
