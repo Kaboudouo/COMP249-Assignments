199f66027aef2ccf81d36553f441d878549b3a8d
Kaboudouo <chotto60@hotmail.com>
2023-03-09 18:32:36 -0500

    Created Assignment 3 base

---
+++ Assignment3/part1/A3_part1.java
@@ -0,0 +1,8 @@
+package Assignment3.part1;
+
+public class A3_part1{
+    public static void main (String[] args){
+
+    }
+    
+}
---
+++ Assignment3/part2/A3_part2.java
@@ -0,0 +1,9 @@
+package Assignment3.part2;
+
+public class A3_part2{
+    public static void main (String[] args){
+
+    }
+    
+}
+
