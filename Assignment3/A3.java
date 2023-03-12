7908675e9a297c2dd003aea305800dd247e99a23
Kaboudouo <chotto60@hotmail.com>
2023-03-11 22:34:03 -0500

    Completed Book Syntax Check, Next: Semantics check

--- Assignment3/A3.java
+++ Assignment3/A3.java
@@ -13,8 +13,119 @@ import java.io.IOException;
 import java.io.BufferedInputStream;
 import java.io.File;
 
+class TooFewFieldsException extends Exception{
+    public TooFewFieldsException(String record){
+        System.out.println("\nThis record has too FEW fields: \n" + record);
+    }
+}
+
+class TooManyFieldsException extends Exception{
+    public TooManyFieldsException(String record){
+        System.out.println("\nThis record has too MANY fields: \n" + record);
+    }
+}
+
+
+class MissingFieldException extends Exception{
+    public MissingFieldException(String record, String fieldCodes){
+        System.out.println("\nThis record is missing fields: \n" + record + "  Missing Field Code: " + fieldCodes);
+    }
+}
+
+class UnkownGenreException extends Exception{
+    public UnkownGenreException(String record){
+        System.out.println("\nInvalid genre: \n" + record);
+    }
+}
+
+
+
 public class A3 {
-    public static void do_part1(){
+
+    static void validateFields(String book, String[] fields){
+        System.out.println("\n" + book);
+    }   
+
+    static void validateSyntax(String book){
+        int quoteCount = 0;
+        int fieldCount = 0;
+
+        String[] fields = {"", "", "", "", "", ""};
+        for (char c : book.toCharArray()){
+            if (c == '"'){
+                quoteCount++;
+                continue;
+            }
+            
+            if (c == ',' && quoteCount != 1){
+                fieldCount++;
+                if (fieldCount > 5){
+                    break;
+                }
+                continue;
+            }
+
+            fields[fieldCount] += String.valueOf(c);
+        }
+        
+        try {
+            if (fieldCount > 5){
+                throw new TooManyFieldsException(book);
+            } else if (fieldCount < 5){
+                throw new TooFewFieldsException(book);
+            }
+            String missingFieldCode = "";
+            for (int i = 0; i < fields.length; i++){
+                // System.out.print(i + ": " + fields[i] + " ");
+                if( fields[i] == ""){
+                    missingFieldCode += i;
+                }
+            }
+            if (missingFieldCode != ""){
+                throw new MissingFieldException(book, missingFieldCode);
+            }
+
+            boolean validGenre = false;
+            String[] genreCodes = {"CCB","HCB", "MTV", "MRB", "NEB", "OTR", "SSM", "TPA"};
+            for (int i = 0; i < genreCodes.length; i++){
+                if (fields[4].equals(genreCodes[i])){
+                    validGenre = true;
+                    break;
+                }
+            }
+            if (!validGenre){
+                throw new UnkownGenreException(book);
+            }
+            // Any field array we return now contains a syntactically correct book
+            validateFields(book, fields);
+
+        } catch(TooManyFieldsException e){
+        } catch (TooFewFieldsException e){
+        } catch (MissingFieldException e){
+        } catch (UnkownGenreException e){
+        }
+    }
+
+    static void checkBooks(String path){
+        Scanner bookScanner = null;
+
+        try {
+            InputStream bookStream = new FileInputStream("./Assignment3/docs/"+path);
+            new BufferedInputStream(bookStream);
+            bookScanner = new Scanner (bookStream);
+        }
+        catch (FileNotFoundException e){
+            System.out.println("Could not find file: " + path);
+            return;
+        }
+
+        while (bookScanner.hasNextLine()){
+            validateSyntax(bookScanner.nextLine());
+        }
+        bookScanner.close();
+    }
+
+    static void do_part1(){
         // Pull file from path
         Scanner scanner = null;
         try {
@@ -22,7 +133,7 @@ public class A3 {
             new BufferedInputStream(inputStream);
             scanner = new Scanner (inputStream);
         } catch (FileNotFoundException e){
-            System.out.println("Could not find file.");
+            System.out.println("Could not find input name file.");
             System.exit(0);
         }
 
@@ -31,9 +142,13 @@ public class A3 {
         String[] input_files = new String[n];
         scanner.nextLine();
         for (int i = 0; i < n; i++){
-            input_files[i] = scanner.next();
-            System.out.println(input_files[i]);
+            if (scanner.hasNextLine()){
+                input_files[i] = scanner.nextLine();
+                checkBooks(input_files[i]);
+            }
         }
+
+        //checkBooks("books2010.csv.txt");
     }
 
     public static void main (String[] args){
