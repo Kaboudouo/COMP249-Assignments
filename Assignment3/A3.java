d0c99b60ab12dc2b05610d7ef6dc5405b19966fe
Kaboudouo <chotto60@hotmail.com>
2023-03-13 08:34:57 -0400

    Files are now created and written to (Syntax + Genres), Next > Semantics

--- Assignment3/A3.java
+++ Assignment3/A3.java
@@ -15,68 +15,149 @@ import java.io.File;
 
 class TooFewFieldsException extends Exception{
     public TooFewFieldsException(String record){
-       System.out.println("\nThis record has too FEW fields: \n" + record);
+       // System.out.println("\nThis record has too FEW fields: \n" + record);
     }
 }
 
 class TooManyFieldsException extends Exception{
     public TooManyFieldsException(String record){
-       System.out.println("\nThis record has too MANY fields: \n" + record);
+       // System.out.println("\nThis record has too MANY fields: \n" + record);
     }
 }
 
 class MissingFieldException extends Exception{
-    public MissingFieldException(String record, String fieldCodes){
-       System.out.println("\nThis record is missing fields: \n" + record + "  Missing Field Code: " + fieldCodes);
+    public MissingFieldException(String record){
+       // System.out.println("\nThis record is missing fields: \n" + record);
     }
 }
 
 class UnkownGenreException extends Exception{
     public UnkownGenreException(String record){
-      System.out.println("\nInvalid genre: \n" + record);
+      // System.out.println("\nInvalid genre: \n" + record);
     }
 }
 
 class BadPriceException extends Exception{
     public BadPriceException(String record){
-       System.out.println("\nInvalid Price: \n" + record);
+       // System.out.println("\nInvalid Price: \n" + record);
     }
 }
 
 class BadYearException extends Exception{
     public BadYearException(String record){
-       System.out.println("\nInvalid Year: \n" + record);
+       // System.out.println("\nInvalid Year: \n" + record);
     }
 }
 class BadIsbn10Exception extends Exception{
     public BadIsbn10Exception(String record){
-        System.out.println("\nBad ISBN-10: \n" + record);
+        // System.out.println("\nBad ISBN-10: \n" + record);
     }
 }
 class BadIsbn13Exception extends Exception{
     public BadIsbn13Exception(String record){
-        System.out.println("\nBad ISBN-13: \n" + record);
+        // System.out.println("\nBad ISBN-13: \n" + record);
     }
 }
 
 
 public class A3 {
+    static PrintWriter cartoonsWriter = null;
+    static PrintWriter hobbiesWriter = null;
+    static PrintWriter moviesWriter = null;
+    static PrintWriter musicWriter = null;
+    static PrintWriter nostalgiaWriter = null;
+    static PrintWriter oldWriter = null;
+    static PrintWriter sportWriter = null;
+    static PrintWriter trainsWriter = null;
+    static PrintWriter syntaxWriter = null;
 
-    static void writeToCsv(String book, String genre){
-        // System.out.println("\n" + book);
+    static String oldPath = "";
+
+    static void manipulatePart1Writers(boolean closeAll){
+        if (closeAll){
+            cartoonsWriter.close();
+            hobbiesWriter.close();
+            moviesWriter.close();
+            musicWriter.close();
+            nostalgiaWriter.close();
+            oldWriter.close();
+            sportWriter.close();
+            trainsWriter.close();
+            syntaxWriter.close();
+        }else {
+            try{
+                cartoonsWriter = new PrintWriter(new FileOutputStream("./Assignment3/output/Cartoons_Comics.csv"));
+                hobbiesWriter = new PrintWriter(new FileOutputStream("./Assignment3/output/Hobbies_Collectibles.csv"));
+                moviesWriter = new PrintWriter(new FileOutputStream("./Assignment3/output/Movies_TV_Books.csv"));
+                musicWriter = new PrintWriter(new FileOutputStream("./Assignment3/output/Music_Radio_Books.csv"));
+                nostalgiaWriter = new PrintWriter(new FileOutputStream("./Assignment3/output/Nostalgia_Electronic_Book.csv"));
+                oldWriter = new PrintWriter(new FileOutputStream("./Assignment3/output/Old_Time_Radio.csv"));
+                sportWriter = new PrintWriter(new FileOutputStream("./Assignment3/output/Sport_Memorabilia.csv"));
+                trainsWriter = new PrintWriter(new FileOutputStream("./Assignment3/output/Trains_Planes_Automobiles.csv"));
+                syntaxWriter = new PrintWriter(new FileOutputStream("./Assignment3/output/syntax_error_file.txt"));
+            } catch (Exception e){
+                System.out.println("Could not find one or more of the output files (Part 1)");
+                System.exit(0);
+            }
+
+        }
     }
 
-    static String[] createFields(String book){
+    static void writeToSyntaxFile(String book, String path, String msg){
+        if (!oldPath.equals(path)){
+            oldPath = path;
+            syntaxWriter.println("Syntax Error in File: " + path + "\n======================================");
+        }
+        syntaxWriter.println("Error: " + msg);
+        syntaxWriter.println("Record: " + book + "\n");
+    }
+
+    static void writeToCsv(String book, String genre){
+        switch (genre){
+            case "CCB":
+                cartoonsWriter.println(book);
+                break;
+            case "HCB":
+                hobbiesWriter.println(book);
+                break;
+            case "MTV":
+                moviesWriter.println(book);
+                break;
+            case "MRB":
+                musicWriter.println(book);
+                break;
+            case "NEB":
+                nostalgiaWriter.println(book);
+                break;
+            case "OTR":
+                oldWriter.println(book);
+                break;
+            case "SSM":
+                sportWriter.println(book);
+                break;
+            case "TPA":
+                trainsWriter.println(book);
+                break;
+            default:
+                System.out.println("Somehow an invalid genre slipped past...");
+                System.exit(0);
+        }
+    }
+
+    static String[] createFields(String book, String path){
         String[] fields = {"", "", "", "", "", ""};
         int quoteCount = 0;
         int fieldCount = 0;
 
         for (char c : book.toCharArray()){
+
+            // Is title is quotes check
             if (c == '"'){
                 quoteCount++;
                 continue;
             }
             
+            // Counts how many commas have passed, thus counting the number of fields except if comma is in quoted title
             if (c == ',' && quoteCount != 1){
                 fieldCount++;
                 if (fieldCount > 5){
@@ -84,6 +165,13 @@ public class A3 {
                 }
                 continue;
             }
+
+            // Eliminates extra spaces past author field as to not cause type errors later
+            if (c == ' ' && fieldCount > 1){
+                continue;
+            }
+
+            // Reconstruct field from char
             fields[fieldCount] += String.valueOf(c);
         }
 
@@ -94,27 +182,56 @@ public class A3 {
                 throw new TooFewFieldsException(book);
             }
         } catch(TooManyFieldsException e){
+            writeToSyntaxFile(book, path, "Too Many Fields");
         } catch (TooFewFieldsException e){
+            writeToSyntaxFile(book, path, "Too Few Fields");
         }
         return fields;
     }
 
-    static void validateSyntax(String book){
+    static void validateSyntax(String book, String path){
 
         String[] fields;
 
-        fields = createFields(book);
+        fields = createFields(book, path);
         
+        String missingFieldMsg = "Missing";
+        // "first" boolean is purely for esthetics
         try {
-            String missingFieldCode = "";
+            boolean first = true;
             for (int i = 0; i < fields.length; i++){
-                // System.out.print(i + ": " + fields[i] + " ");
                 if( fields[i] == ""){
-                    missingFieldCode += i;
+                    if (!first){
+                        missingFieldMsg += ",";
+                    }
+                    first = false;
+                    switch(i){
+                        case 0:
+                            missingFieldMsg += " Title";
+                            break;
+                        case 1:
+                            missingFieldMsg += " Author";
+                            break;
+                        case 2:
+                            missingFieldMsg += " Price";
+                            break;
+                        case 3:
+                            missingFieldMsg += " ISBN";
+                            break;
+                        case 4:
+                            missingFieldMsg += " Genre";
+                            break;
+                        case 5:
+                            missingFieldMsg += " Year";
+                            break;
+                        default:
+                            System.out.println("Out of Range Error occured by now...");
+                            System.exit(0);
+                    }
                 }
             }
-            if (missingFieldCode != ""){
-                throw new MissingFieldException(book, missingFieldCode);
+            if (missingFieldMsg != "Missing"){
+                throw new MissingFieldException(book);
             }
 
             boolean validGenre = false;
@@ -131,7 +248,9 @@ public class A3 {
             // Any field array we return now contains a syntactically correct book
             writeToCsv(book, fields[4]);
         } catch (MissingFieldException e){
+            writeToSyntaxFile(book, path, missingFieldMsg);
         } catch (UnkownGenreException e){
+            writeToSyntaxFile(book, path, "Invalid Genre");
         }
     }
 
@@ -149,7 +268,7 @@ public class A3 {
         }
 
         while (bookScanner.hasNextLine()){
-            validateSyntax(bookScanner.nextLine());
+            validateSyntax(bookScanner.nextLine(), path);
         }
         bookScanner.close();
     }
@@ -187,7 +306,7 @@ public class A3 {
 
     // Validates each field of the syntactically correct books
     static void validateFields(String book, String originPath){
-        String[] fields = createFields(book);
+        String[] fields = createFields(book, originPath);
 
         try{
             // Price check
@@ -253,7 +372,10 @@ public class A3 {
     }
 
     public static void main (String[] args){
+        manipulatePart1Writers(false);
         do_part1();
-        do_part2();
+        manipulatePart1Writers(true);
+
+        //do_part2();
     }
 }
